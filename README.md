b87e0c8378504e948ca1f9a9b922b296d4a72d35
Ren Wang <iamwrm@gmail.com>
2023-09-09 15:37:07 +0800

    init project

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # tired_server
+
+This program detects if your server has been idle for a certain amount of time and shuts it down if it has.
+
+## implementation
+
+- systemstat is used to get the load average
+https://github.com/valpackett/systemstat
+- when the filter is triggered, the executor is called. You can run any bash scripts such as shutdown or reboot (sudo required), or send an API call using curl.
+
+
+## Config example
+
+```toml
+
+[trigger]
+
+record_interval = "60s"
+grace_period = "10min"
+threshold = "0.5"
+
+[executor]
+
+bash = "echo `system has been idle for 10 minutes`"
+
+
+```
+
+## sqlite query used
+
+```sql
+SELECT  MAX(load_5)
+FROM    records
+WHERE   update_ts > datetime('now', '-10 minutes')
+```
+
+```python
+
+rows = conn.execute(query)
+if len(rows) ==0:
+    print("not enough data points")
+    return False
+else:
+    return rows[0][0] < threshold
+
+```
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use systemstat::{saturating_sub_bytes, Platform, System};
+
+fn main() {
+    let sys = System::new();
+
+    match sys.load_average() {
+        Ok(loadavg) => println!(
+            "\nLoad average: {} {} {}",
+            loadavg.one, loadavg.five, loadavg.fifteen
+        ),
+        Err(x) => println!("\nLoad average: error: {}", x),
+    }
+
+    let connection = sqlite::open(":memory:").unwrap();
+
+    let query = "
+    CREATE TABLE users (name TEXT, age INTEGER);
+    INSERT INTO users VALUES ('Alice', 42);
+    INSERT INTO users VALUES ('Bob', 69);
+    ";
+    connection.execute(query).unwrap();
+
+    // print all roles
+    let mut statement = connection.prepare("SELECT * FROM users").unwrap();
+    while let sqlite::State::Row = statement.next().unwrap() {
+        println!("name = {}", statement.read::<String, _>(0).unwrap());
+        println!("age = {}", statement.read::<i64, _>(1).unwrap());
+    }
+}
